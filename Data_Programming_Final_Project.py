e471a3931306f4ddd04036b036783ede4773ba83
brandaovh <156470308+brandaovh@users.noreply.github.com>
2024-04-10 18:31:09 -0400

    Update Data_Programming_Final_Project.py

--- Data_Programming_Final_Project.py
+++ Data_Programming_Final_Project.py
@@ -152,10 +152,10 @@ from bson.objectid import ObjectId
 # Connect to your MongoDB server
 client = pymongo.MongoClient("mongodb+srv://brandaovh:654123@cluster0.qswfw8s.mongodb.net/?retryWrites=true&w=majority")
 # Select the database and collection
-db = client['imdb']
-collection = db['imdb_movies']
+db = client["imdb"]
+collection = db["imdb_movies"]
 
-# Define Layout of App and add server
+# Define Layout of App
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
                 suppress_callback_exceptions=True)
@@ -175,7 +175,8 @@ app.layout = html.Div([
 
 # Display Datatable with data from Mongo database
 @app.callback(Output('mongo-datatable', component_property='children'),
-              Input('interval_db', component_property='n_intervals'))
+              Input('interval_db', component_property='n_intervals')
+              )
 def populate_datatable(n_intervals):
     # Convert the Collection (table) date to a pandas DataFrame
     df = pd.DataFrame(list(collection.find()))
@@ -185,15 +186,16 @@ def populate_datatable(n_intervals):
     df['genre_ids'] = df['genre_ids'].apply(lambda x: ', '.join(map(str, x)))
 
     return [
-        dash_table.DataTable(
-            id='our-table',
-            data=df.to_dict('records'),
-            columns=[{'id': p, 'name': p, 'editable': False} if p == '_id'
-                     else {'id': p, 'name': p, 'editable': True}
-                     for p in df],
-            page_size=10,  # Only display 10 rows at a time
-            style_table={'overflowX': 'auto', 'maxHeight': '500px', 'overflowY': 'auto'}
-        )
+       dash_table.DataTable(
+    id='our-table',
+    data=df.to_dict('records'),
+    columns=[{'id': p, 'name': p, 'editable': False} if p == '_id'
+             else {'id': p, 'name': p, 'editable': True}
+             for p in df],
+    page_size=10,  # Only display 10 rows at a time
+    style_table={'overflowX': 'auto', 'maxHeight': '500px', 'overflowY': 'auto'}
+)
+,
     ]
 
 
@@ -245,7 +247,7 @@ def update_d(cc, tabledata):
 
         # Build the Plots
         pie_fig = px.pie(top_vote_count, values='vote_count', names='title', color_discrete_sequence=px.colors.sequential.RdBu)
-        hist_fig = px.histogram(top_popularity, x='title', y='popularity')
+        hist_fig = px.histogram(top_popularity, x='title', y='popularity', color='title')
 
         # Update layout for better visualization
         pie_fig.update_layout(title_text='Top 5 Movies by Vote Count', title_x=0.5)
